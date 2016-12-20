74ea827bb557212e02bb62a151aba5f413710e25
Peter De Genaro <pdegenaro@gmail.com>
2016-12-20 14:59:56 -0500

    add ajax

--- dominodes.js
+++ dominodes.js
@@ -44,11 +44,11 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	document.callbacks = [];
-
 	const DOMNodeCollection = __webpack_require__(1);
 
-	Window.prototype.$d = function(arg) {
+	document.callbacks = [];
+
+	window.$d = (arg) => {
 	  let selected = [];
 	  let callbacks = [];
 
@@ -61,6 +61,42 @@
 	  }
 
 	  return new DOMNodeCollection(selected);
+	};
+
+	$d.extend = (base, ...objects) => {
+	  objects.forEach((object) => {
+	    Object.keys(object).forEach((key) => {
+	      base[key] = object[key];
+	    });
+	  });
+
+	  return base;
+	};
+
+	$d.ajax = (options) => {
+	  let request = new XMLHttpRequest();
+
+	  let defaults = {
+	    contentType: 'application/x-www-form-urlencoded: charset=UTF-8',
+	    method: "GET",
+	    url: "",
+	    success: () => {},
+	    error: () => {},
+	    data: {}
+	  };
+
+	  options = $d.extend(defaults, options);
+
+	  request.open(options.method, options.url, true);
+	  request.onload = e => {
+	    if (request.status === 200) {
+	      options.success(request.response);
+	    } else {
+	      options.error(request.response);
+	    }
+	  };
+
+	  request.send(JSON.stringify(options.data));
 	};
 
 	document.addEventListener("DOMContentLoaded", () => {
